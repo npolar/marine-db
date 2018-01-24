a683d40ce8ea2e3e68e917cd584c4a55b4a262f1
cnrdh <cnrdh@users.noreply.github.com>
2018-01-24 11:36:01 +0100

    Added

---
+++ node/taxonFixes.mjs
@@ -0,0 +1,15 @@
+export const taxonFixes = [
+  // [ unaccepted, accepted ]
+  ['Eupagurus', 'Pagurus'],
+  ['Gymnodinium gaelatum', 'Gymnodinium galeatum'],
+  ['Heterorhabdus compactus', 'Paraheterorhabdus compactus'],
+  ['Sagitta elegans', 'Parasagitta elegans'],
+  ['Sagitta maxima', 'Pseudosagitta maxima'],
+  ['Triconia conifera', 'Oncaea conifera'],
+  ['Disco fiordicus', 'Pertsovius fjordicus'],
+  ['Arrhis phyllony', 'Arrhis phyllonyx'],
+  ['Monstrilloidae', 'Monstrillidae'], // https://www.gbif.org/species/9127
+  ['Myrionecta rubra', 'Mesodinium rubrum'],
+  ['Erythrops erythrophthalma', 'Erythrops erythrophthalmus'] //http://www.marinespecies.org/aphia.php?p=taxdetails&id=852968
+  // Gammaridea
+];
