008fb4cfe5de4acee4c931a5afc9d730918d4a0e
cnrdh <cnrdh@users.noreply.github.com>
2018-02-23 10:02:01 +0100

    Updated

--- node/taxonFixes.mjs
+++ node/taxonFixes.mjs
@@ -1,15 +1,49 @@
+// @todo Remove any of these from *-accepted, e.gf: Tharybis groenlandicus
 export const taxonFixes = [
   // [ unaccepted, accepted ]
+  ['Turbellaria', 'Tubularia'], // typo
+  ['Hydromedusae', 'Hydrozoa'],
+  ['Ciliate', 'Ciliata'],
   ['Eupagurus', 'Pagurus'],
-  ['Gymnodinium gaelatum', 'Gymnodinium galeatum'],
+  ['Chrysophyta', 'Chrysophyceae'], // class
+  ['Mysidacea', 'Mysida'], // order
+  ['Dinophyta', 'Dinophyceae'],
+  ['Heterokontophyta', 'Ochrophyta'],
+
+  ['Strombidium estatum', 'Strombidium'], // genus, unknown species epithet ("estatum")
+  ['Gymnodinium flagellare', 'Gymnodinium'], // genus: https://www.gbif.org/species/8661776
+
+  // Chromista: genus is correct?
+  //["Dinobryon statospores", "Gymnodinium flagellare", "Nitzschia arctica", "Strombidium spiralissloneczko", "Teleaulax marina", "Thalassiosira borealis", "Thalassiosira borealis", "Thalassiosira pacyficaphoto"]
+
+
+  ['Gymnodinium gaelatum', 'Gymnodinium galeatum'], //typo
+  ['Gymnodinium gracilientum', 'Gymnodinium gracilentum'],
+  ['Gymnodinium pulchellum', 'Takayama pulchella'], // synonym https://www.gbif.org/species/7516124
   ['Heterorhabdus compactus', 'Paraheterorhabdus compactus'],
   ['Sagitta elegans', 'Parasagitta elegans'],
   ['Sagitta maxima', 'Pseudosagitta maxima'],
   ['Triconia conifera', 'Oncaea conifera'],
   ['Disco fiordicus', 'Pertsovius fjordicus'],
-  ['Arrhis phyllony', 'Arrhis phyllonyx'],
-  ['Monstrilloidae', 'Monstrillidae'], // https://www.gbif.org/species/9127
+  ['Arrhis phyllony', 'Arrhis phyllonyx'], //typo
+  ['Monstrilloidae', 'Monstrillidae'], // typo? https://www.gbif.org/species/9127
   ['Myrionecta rubra', 'Mesodinium rubrum'],
-  ['Erythrops erythrophthalma', 'Erythrops erythrophthalmus'] //http://www.marinespecies.org/aphia.php?p=taxdetails&id=852968
-  // Gammaridea
+  ['Erythrops erythrophthalma', 'Erythrops erythrophthalmus'], //typo? http://www.marinespecies.org/aphia.php?p=taxdetails&id=852968
+  ['Scolocithricella minor', 'Scolecithricella minor'], //typo
+  ['Monoculoides pacardi', 'Monoculodes packardi'], //typo
+  ['Undeuchaeta spectabilis', 'Pseudochirella spectabilis'],
+  ['Tharybis groenlandicus', 'Tharybis groenlandica'],
+  ['Lubbockia brevis', 'Homeognathia brevis'],
+  // Asteroidea?
+  // Gammaridea?
+  ['Chaetoceros lacinosus', 'Chaetoceros laciniosus'], // phylum Ochrophyta
+  ['Phalacroma rotundata', 'Phalacroma rotundatum'], // phylum Myzozoa
+  ['Uronema marina', 'Uronema marinum'], // phylum Ciliophora
+  ['Nitzschia granii', 'Pseudo-nitzschia granii'],
+  ['Dinophysis rotundata', 'Phalacroma rotundatum'],
+  ['Tintinnus inquilinum', 'Tintinnus inquilinus'], //typo?
+  ['Pleurochrysis carterae', 'Chrysotila carteri'],
+  ['Prorocentrum minimum', 'Prorocentrum cordatum'],
+  ['Chaetoceros terens', 'Chaetoceros teres'] // typo
+
 ];
