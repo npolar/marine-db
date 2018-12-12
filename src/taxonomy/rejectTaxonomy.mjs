56d4bffa442cd5376e0d1b85f0838d918d3b3c82
cnrdh <conrad@npolar.no>
2018-12-12 15:43:54 +0100

    Taxonomy, now under darwin-core

--- src/taxonomy/rejectTaxonomy.mjs
+++
@@ -1,9 +0,0 @@
-export const rejectTaxonomy = [
-  (t) => ["Cyst", "Cyst sp.",
-    "Fecal pelet",
-    "Indeterm spores",
-    "Microcystis",
-    "Spores", "Spora",
-    "Unidentified cells", "Unknown"].includes(String(t.taxon)),
-  (t) => String(t.sample).length < 4
-];
